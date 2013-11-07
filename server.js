b3cd8224e26d1ad40c7589074f3acc5167545313
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2013-11-07 13:37:35 -0700

    Created js files to practice for nodebeginner.org

---
+++ server.js
@@ -0,0 +1,35 @@
+/*var http = require("http");
+
+function start () {
+	function onRequest(request, response) {
+  console.log("Request received.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World, your request was received.");
+  response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
+}
+exports.start = start;*/
+
+
+var http = require("http");
+var url = require("url");
+
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
